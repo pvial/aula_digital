96e5f75a9c31f0deffa6b6db2bc1df19d8d2a239
Raghu Betina <raghu@firstdraft.com>
2018-05-31 15:03:47 +0000

    Direct association between nivels and objetivos

--- app/models/nivel.rb
+++ app/models/nivel.rb
@@ -1,6 +1,8 @@
 class Nivel < ApplicationRecord
   # Direct associations
 
+  has_many   :objetivos
+
   # Indirect associations
 
   # Validations
--- app/models/objetivo.rb
+++ app/models/objetivo.rb
@@ -1,6 +1,10 @@
 class Objetivo < ApplicationRecord
   # Direct associations
 
+  belongs_to :year,
+             :class_name => "Nivel",
+             :foreign_key => "nivel_id"
+
   belongs_to :eje
 
   has_many   :indicators,
