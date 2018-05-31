25f0a40eb222dd371028db5884fd7eebdf413274
Raghu Betina <raghu@firstdraft.com>
2018-05-31 15:03:47 +0000

    Direct association between indicators and indicators

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,6 +1,13 @@
 class Indicator < ApplicationRecord
   # Direct associations
 
+  belongs_to :next_indicator,
+             :class_name => "Indicator"
+
+  has_one    :coming_from_indicator,
+             :class_name => "Indicator",
+             :foreign_key => "next_indicator_id"
+
   belongs_to :preceding_indicator,
              :class_name => "Indicator",
              :foreign_key => "pre_indicator_id"
