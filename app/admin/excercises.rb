26e92276f2d56e413f4625cdf0447fa2c9ada2ce
Raghu Betina <raghu@firstdraft.com>
2018-05-31 15:04:04 +0000

    Whitelist columns in Excercise for ActiveAdmin

--- app/admin/excercises.rb
+++ app/admin/excercises.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Excercise do
+
+ permit_params :contenido_id, :skill, :dificultad, :imagen, :question, :indicator_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
