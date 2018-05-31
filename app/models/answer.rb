b71a1f22bcc25e90efffedeffd20331f807d85a3
Raghu Betina <raghu@firstdraft.com>
2018-05-31 15:03:37 +0000

    Direct association between excercises and answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ApplicationRecord
   # Direct associations
 
+  belongs_to :excercise
+
   # Indirect associations
 
   # Validations
--- app/models/excercise.rb
+++ app/models/excercise.rb
@@ -1,6 +1,9 @@
 class Excercise < ApplicationRecord
   # Direct associations
 
+  has_one    :answer,
+             :dependent => :destroy
+
   has_many   :tries
 
   # Indirect associations
