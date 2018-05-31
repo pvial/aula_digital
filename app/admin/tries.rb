922e3bcb86e25bca16dd8bcb172604ca28362607
Raghu Betina <raghu@firstdraft.com>
2018-05-31 15:04:20 +0000

    Whitelist columns in Try for ActiveAdmin

--- app/admin/tries.rb
+++ app/admin/tries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Try do
+
+ permit_params :user_id, :excercise_id, :user_answer, :correct, :active
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
